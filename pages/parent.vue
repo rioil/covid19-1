754823a1f4e0e63d0d43d96be82cd9a120e32dee
Yusei Ito <yuseikun@Outlook.jp>
2020-03-17 15:57:04 +0900

    Modify: edit link

--- pages/parent.vue
+++ pages/parent.vue
@@ -41,7 +41,9 @@ export default {
           body: [
             this.$t('各保健所にご相談ください'),
             this.$t('「新型コロナウイルス感染症にかかる相談窓口について」'),
-            '<a href="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html" target="_blank" rel="noopener">https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html</a>'
+            '<a href="https://www.pref.shiga.lg.jp/ippan/kenkouiryouhukushi/yakuzi/310356.html" target="_blank" rel="noopener">https://www.pref.shiga.lg.jp/ippan/kenkouiryouhukushi/yakuzi/310356.html</a>',
+            this.$t('電話相談') +
+              '<br><a href="http://www.pref.shiga.lg.jp/ippan/kenkouiryouhukushi/yakuzi/310358.html target="_blank" rel="noopener">http://www.pref.shiga.lg.jp/ippan/kenkouiryouhukushi/yakuzi/310358.html  </a>'
           ].join('<br />')
         },
         {
