53282aa86c848131c9e16135caa34b28ed0f8d8b
cynage <38454938+cynagenautes@users.noreply.github.com>
2020-03-30 09:38:40 +0900

    Fix: about.vue (#35)

--- pages/about.vue
+++ pages/about.vue
@@ -162,7 +162,7 @@
       }}
       <i18n :path="$t('詳しくは、{githubRepo}をご確認ください。')">
         <a
-          href="https://github.com/tokyo-metropolitan-gov/covid19"
+          href="https://github.com/Shiga-pref-org/covid19"
           target="_blank"
           rel="noopener"
           place="githubRepo"
